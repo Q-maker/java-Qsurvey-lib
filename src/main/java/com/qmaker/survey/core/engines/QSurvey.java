eefcd2125530499691633943eb66eb835942e033
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2018-12-16 19:52:09 +0000

    one more step

--- src/main/java/com/qmaker/survey/core/engines/QSurvey.java
+++ src/main/java/com/qmaker/survey/core/engines/QSurvey.java
@@ -12,6 +12,7 @@ import com.qmaker.survey.core.entities.Survey;
 import com.qmaker.survey.core.interfaces.PersistenceUnit;
 import com.qmaker.survey.core.interfaces.PushProcess;
 import com.qmaker.survey.core.interfaces.Pusher;
+import com.qmaker.survey.core.interfaces.SurveyStateListener;
 import com.qmaker.survey.core.utils.MemoryPersistenceUnit;
 import com.qmaker.survey.core.utils.PayLoad;
 import com.qmaker.survey.core.pushers.FileIoPusher;
@@ -289,20 +290,6 @@ public class QSurvey implements QRunner.StateListener, PushExecutor.ExecutionSta
 //            }
 //        }
 //    }
-
-    public interface SurveyStateListener {
-        int STATE_PREPARED = 0x00000001,
-                STATE_STARTED = 0x00000010,
-                STATE_TIME_TICK = 0x00000100,
-                STATE_EXERCISE_CHANGED = 0x00001000,
-                STATE_TIME_OUT = 0x00010000,
-                STATE_CANCELED = 0x00100000,
-                STATE_RESET = 0x01000000,
-                STATE_COMPLETED = 0x10000000,
-                STATE_FINISH = STATE_COMPLETED | STATE_CANCELED | STATE_TIME_OUT;
-
-        void onSurveyStateChanged(int state, Survey survey, PayLoad payLoad);
-    }
 
     final static HashMap<String, Pusher> pusherMap = new HashMap();
 
---
+++ src/main/java/com/qmaker/survey/core/interfaces/SurveyStateListener.java
@@ -0,0 +1,18 @@
+package com.qmaker.survey.core.interfaces;
+
+import com.qmaker.survey.core.entities.Survey;
+import com.qmaker.survey.core.utils.PayLoad;
+
+public interface SurveyStateListener {
+    int STATE_PREPARED = 0x00000001,
+            STATE_STARTED = 0x00000010,
+            STATE_TIME_TICK = 0x00000100,
+            STATE_EXERCISE_CHANGED = 0x00001000,
+            STATE_TIME_OUT = 0x00010000,
+            STATE_CANCELED = 0x00100000,
+            STATE_RESET = 0x01000000,
+            STATE_COMPLETED = 0x10000000,
+            STATE_FINISH = STATE_COMPLETED | STATE_CANCELED | STATE_TIME_OUT;
+
+    void onSurveyStateChanged(int state, Survey survey, PayLoad payLoad);
+}
